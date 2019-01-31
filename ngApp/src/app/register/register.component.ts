9d86f86d7d8ca505e058364b024df5cfbfa1d698
Joel R. Paderagao <joel.paderagao@outlook.com>
2019-02-01 07:54:57 +0800

    add interface user

--- ngApp/src/app/register/register.component.ts
+++ ngApp/src/app/register/register.component.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import {AuthService} from '../auth.service'
 import {Router} from '@angular/router'
-
+import {User} from '../models/user'
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-  registerUserData = {}
+  registerUserData : User
   constructor(private _auth:AuthService,
              private _router:Router) { }
 

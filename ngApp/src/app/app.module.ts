004ecb224959e72532558bc68947d9849e10f691
Paderagao <joel.r.paderagao@accenture.com>
2019-02-06 13:23:17 +0800

    added core api

--- ngApp/src/app/app.module.ts
+++ ngApp/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EventsComponent } from './events/events.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { SpecialEventsComponent } from './special-events/special-events.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EventsComponent,
+    LoginComponent,
+    RegisterComponent,
+    SpecialEventsComponent
   ],
   imports: [
     BrowserModule,
